18d7c7e2407725440977605b29fc82dc1a0345cb
Dodistywn <dodisetya000@gmail.com>
2023-03-23 22:36:47 +0700

    merubah file js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 let angka=12;
-
  
 if (angka%2==0) 
 {
-   console.log("Angka = "+angka);
+   console.log("Angka : "+ angka);
    console.log("Angka adalah bilangan genap");
-}
-else
-{
-   console.log("Angka = "+angka);
+}else{
+   console.log("Angka : "+ angka);
    console.log("Angka adalah bilangan ganjil");
 }
