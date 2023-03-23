9118db1585a9551b8a19fe6c576d0d2e081a2da6
Dodistywn <dodisetya000@gmail.com>
2023-03-23 22:32:45 +0700

    menambahkan file html dan file js

---
+++ script.js
@@ -0,0 +1,13 @@
+let angka=12;
+
+ 
+if (angka%2==0) 
+{
+   console.log("Angka = "+angka);
+   console.log("Angka adalah bilangan genap");
+}
+else
+{
+   console.log("Angka = "+angka);
+   console.log("Angka adalah bilangan ganjil");
+}
