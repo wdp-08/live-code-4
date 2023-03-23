9233c30d75fdf0cf3d8e2d0aaee14cd777c0f03a
AdamBagusP <adampdn01.ab@gmail.com>
2023-03-23 22:45:05 +0700

    add odd even check

---
+++ script4.js
@@ -0,0 +1,10 @@
+const num = prompt("Masukkan Angka: ");
+
+if(num % 2 == 0) {
+    console.log("Genap");
+}
+else if (num % 2 == 1){
+    console.log("Ganjil");
+} else {
+    console.log("Invalid Data");
+}
