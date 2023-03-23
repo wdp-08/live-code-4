61294802b5fc14090ad0137b031871da4f3d2b70
AdiPutra <madiputra0107@gmail.comgit config --global user.name madiputra0107@gmail.com>
2023-03-23 22:46:19 +0700

    hay

---
+++ js.js
@@ -0,0 +1,16 @@
+const nomor = [2, 3, 20, 21];
+let genap = [];
+	let ganjil = [];
+	let i = 0;
+
+		for(i; i <nomor.length; i++){
+			return "Invalid Data";
+		}
+        if (nomor[i] % 2 == 0) {
+            genap.push(nomor[i]);
+        }else{
+            ganjil.push(nomor[i]);
+        }
+console.log(genap);
+console.log(ganjil);
+
